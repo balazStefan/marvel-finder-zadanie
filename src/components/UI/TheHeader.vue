e1cffecfc72471b1b8da9240d3f66acffe31cf6d
stevobko123 <balaz.stefan1996@gmail.com>
2022-01-12 11:35:42 +0100

    zjednodusenie class v headeri pomocou tailwind

--- src/components/UI/TheHeader.vue
+++ src/components/UI/TheHeader.vue
@@ -1,17 +1,11 @@
 <template>
   <div>
-    <header
-      class="mt-2 mr-2 flex justify-end gap-6 lg:flex lg:justify-center lg:align-middle lg:gap-10 lg:mt-2 lg:mr-2"
-    >
-      <router-link
-        to="/home"
-        class="m-1 p-2 bg-white justify-center font-bold align-middle no-underline self-center cursor-pointer border-0 text-center lg:m-2 lg:p-4 lg:bg-white lg:justify-center lg:no-underline lg:self-center lg:font-bold lg:cursor-pointer lg:border-0 lg:text-center text-gray-800"
-        >Home Page</router-link
-      >
+    <header class="mt-2 mr-2 flex justify-end gap-6">
+      <router-link to="/home" class="link-navbar lg:p-4">Home Page</router-link>
       <router-link
         v-if="favoriteHeroes.length"
         to="/favorite"
-        class="m-1 p-2 bg-white justify-center font-bold align-middle no-underline self-center cursor-pointer border-0 text-center lg:m-2 lg:p-4 lg:bg-white lg:justify-center lg:no-underline lg:self-center lg:font-bold lg:cursor-pointer lg:border-0 lg:text-center text-gray-800 lg:rounded"
+        class="link-navbar lg:p-4"
         >Favorite Heroes</router-link
       >
     </header>
